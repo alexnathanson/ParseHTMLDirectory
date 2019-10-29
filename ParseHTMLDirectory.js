d8ae9b08da82cbeb7181be10762461e1da7627af
alexnathanson <alex@alexnathanson.com>
2019-10-29 13:08:01 -0400

    just file names

--- ParseHTMLDirectory.js
+++ ParseHTMLDirectory.js
@@ -1,16 +1,15 @@
 //parses the HTML get request for the files names
+// XMLHttpRequest reference https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/getResponseHeader
 
 class HTMLDirectory{
 	constructor(tempIP,tempDirectory){
 		this.ip = tempIP;
 		this.directory = tempDirectory;
 		this.path = this.ip + this.directory;
-		this.list;
-		//this.scope = this;
+		this.list = [];
 	}
 
 	getDir(){
-	  //based off of https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/getResponseHeader
 	  let client = new XMLHttpRequest();
 	  client.responseType = 'text';
 	  client.open("GET", this.path, true);
@@ -19,24 +18,12 @@ class HTMLDirectory{
 
 	  //the arrow functions maintains the scope
 	  client.onreadystatechange =()=>{
-	    /*if(this.readyState == this.HEADERS_RECEIVED) {
-	      this.response = client;
-	      
-	      console.log(client);
-	      console.log(this.response.responseText);
-
-	      //this.parseList();
-	  	}*/
 	  	if(client.readyState === 4 && client.status === 200) {
 		    //console.log(client.responseText);
 		    this.parseList(client.responseText);
 		  }
 	  };
 	}
-
-	/*set sResponse(arg){
-		this.sResponse = arg;
-	}*/
 
 	parseList(arg){
 		let fileList=arg.split('\n');
@@ -48,10 +35,13 @@ class HTMLDirectory{
 			    //console.log(fileInfo);
 			    for(let f = 0; f<fileInfo.length;f++){
 			    	if(fileInfo[f].includes(this.directory)){
-			    		console.log(fileInfo[f]);
+			    		let len = this.directory.length;
+			    		this.list.push(fileInfo[f].slice(len,fileInfo[f].length));
+			    		//console.log(fileInfo[f].slice(len,fileInfo[f].length));
 			    	}
 			    }
 			}
 		}
+		console.log(this.list);
 	}
 }
