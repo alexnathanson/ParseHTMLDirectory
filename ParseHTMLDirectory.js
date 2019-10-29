20d094306657bba21ad17d884175828fbed97b86
alexnathanson <alex@alexnathanson.com>
2019-10-29 12:55:01 -0400

    parsed

--- ParseHTMLDirectory.js
+++ ParseHTMLDirectory.js
@@ -1,26 +1,57 @@
 //parses the HTML get request for the files names
+
 class HTMLDirectory{
-	constructor(tempDirectory){
+	constructor(tempIP,tempDirectory){
+		this.ip = tempIP;
 		this.directory = tempDirectory;
-		this.list = [];
+		this.path = this.ip + this.directory;
+		this.list;
+		//this.scope = this;
 	}
 
-	getDirectoryList(adr){
+	getDir(){
 	  //based off of https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/getResponseHeader
-	  let client = new XMLHttpRequest(this.directory);
-	  client.open("GET", , true);
+	  let client = new XMLHttpRequest();
+	  client.responseType = 'text';
+	  client.open("GET", this.path, true);
+
 	  client.send();
 
-	  client.onreadystatechange = function() {
-	    if(this.readyState == this.HEADERS_RECEIVED) {
-	      let responseText = client.responseText;
+	  //the arrow functions maintains the scope
+	  client.onreadystatechange =()=>{
+	    /*if(this.readyState == this.HEADERS_RECEIVED) {
+	      this.response = client;
 	      
 	      console.log(client);
-	    }
-	  }
+	      console.log(this.response.responseText);
+
+	      //this.parseList();
+	  	}*/
+	  	if(client.readyState === 4 && client.status === 200) {
+		    //console.log(client.responseText);
+		    this.parseList(client.responseText);
+		  }
+	  };
 	}
 
-	parseDirList(){
+	/*set sResponse(arg){
+		this.sResponse = arg;
+	}*/
 
+	parseList(arg){
+		let fileList=arg.split('\n');
+		//console.log(fileList);
+		for(let i=0;i<fileList.length;i++){
+			//check if the item contains a HREF tag
+			if(fileList[i].includes('HREF')){
+				let fileInfo=fileList[i].split('\"');
+			    //console.log(fileInfo);
+			    for(let f = 0; f<fileInfo.length;f++){
+			    	if(fileInfo[f].includes(this.directory)){
+			    		console.log(fileInfo[f]);
+			    	}
+			    }
+			}
+		}
 	}
 }
---
+++ index.js
@@ -0,0 +1,5 @@
+
+
+let dirList = new HTMLDirectory('http://172.16.1.17','/31D73S000475/Update/');
+
+dirList.getDir();
