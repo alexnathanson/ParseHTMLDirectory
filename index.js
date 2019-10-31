34f742c6f2c9eff3903c2a9864984480a5ac8f60
alexnathanson <alex@alexnathanson.com>
2019-10-31 16:23:05 -0400

    addSpace

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//use webpack or browserify
 
-
-let dirList = new HTMLDirectory('http://172.16.1.17','/31D73S000475/Update/');
+let dirList = new HTMLDirectory('http://172.16.1.17','/31D73S000475/Update/', callIt);
 
 dirList.getDir();
+
+function callIt(){
+	console.log(dirList.list);
+}
